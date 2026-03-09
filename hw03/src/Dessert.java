ec5dd09a005895a69947690b0d4dbc082e4532e7
0bs1dian <wobuzhidaoei1014@163.com>
2026-03-09 16:04:19 +0800

    Java Exercises

--- hw03/src/Dessert.java
+++ hw03/src/Dessert.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class Dessert {
+    private int flavor;
+    private int price;
+    private static int numDesserts = 0;
+
+    public Dessert(int f, int p) {
+        flavor = f;
+        price = p;
+        numDesserts += 1;
+    }
+
+    public void printDessert() {
+        System.out.println(this.flavor + " " + this.price + " " + numDesserts);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("I love dessert!");
+    }
 }
