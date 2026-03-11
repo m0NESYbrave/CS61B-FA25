abcf986d0ada78e4120edcef951f76117d0cc544
0bs1dian <wobuzhidaoei1014@163.com>
2026-03-11 14:50:56 +0800

    Guitar Hero

--- proj_DequeExtra/src/gh2/GuitarString.java
+++ proj_DequeExtra/src/gh2/GuitarString.java
@@ -1,8 +1,7 @@
 package gh2;
 
+import deque.ArrayDeque61B;
 import deque.Deque61B;
-
-// TODO: maybe more imports
 
 //Note: This file will not compile until you complete the Deque61B implementations
 public class GuitarString {
@@ -13,43 +12,40 @@ public class GuitarString {
     private static final double DECAY = .996; // energy decay factor
 
     /* Buffer for storing sound data. */
-    // TODO: uncomment the following line once you're ready to start this portion
-    // private Deque61B<Double> buffer;
+    private Deque61B<Double> buffer;
 
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
-        // TODO: Initialize the buffer with capacity = SR / frequency. You'll need to
-        //       cast the result of this division operation into an int. For
-        //       better accuracy, use the Math.round() function before casting.
-        //       Your should initially fill your buffer with zeros.
+        buffer = new ArrayDeque61B<>();
+        int size = (int) Math.round(SR / frequency);
+        for (int i = 0; i < size; i++) {
+            buffer.addLast(0.0);
+        }
     }
 
 
     /* Pluck the guitar string by replacing the buffer with white noise. */
     public void pluck() {
-        // TODO: Dequeue everything in buffer, and replace with random numbers
-        //       between -0.5 and 0.5. You can get such a number by using:
-        //       double r = Math.random() - 0.5;
-        //
-        //       Make sure that your random numbers are different from each
-        //       other. This does not mean that you need to check that the numbers
-        //       are different from each other. It means you should repeatedly call
-        //       Math.random() - 0.5 to generate new random numbers for each array index.
+        // Better not use enhanced for loop, though it works here.
+        // In Java, if we iterate a list while modifying it like adding/removing,
+        // the program will throw ConcurrentModificationException.
+        for (int i = 0; i < buffer.size(); i++) {
+            buffer.removeFirst();
+            buffer.addLast(Math.random() - 0.5);
+        }
     }
 
     /* Advance the simulation one time step by performing one iteration of
      * the Karplus-Strong algorithm.
      */
     public void tic() {
-        // TODO: Dequeue the front sample and enqueue a new sample that is
-        //       the average of the two multiplied by the DECAY factor.
-        //       **Do not call StdAudio.play().**
+        double front = buffer.removeFirst();
+        double newDouble = DECAY * 0.5 * (front + sample());
+        buffer.addLast(newDouble);
     }
 
     /* Return the double at the front of the buffer. */
     public double sample() {
-        // TODO: Return the correct thing.
-        return 0;
+        return buffer.get(0);
     }
 }
-    // TODO: Remove all comments that say TODO when you're done.
